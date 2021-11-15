fa2c3d4882d2e17426b4a756b4c2e12529695ce2
grubbs39 <69489252+grubbs39@users.noreply.github.com>
2021-11-15 17:32:16 -0600

    Add files via upload

---
+++ probability_of_drops.py
@@ -0,0 +1,17 @@
+class probability_of_drops_math:
+
+    def drop(x, number_of_kills, name):
+        from numpy.random import choice
+        from collections import Counter
+
+        item_drops = 1 / x
+        z = 1 - item_drops
+        item2 = name, "Nothing"
+        probability2 = [item_drops, z]
+        data = []
+        count = Counter(data)
+        for n in range(number_of_kills):
+            item1 = choice(item2, p = probability2)
+            data = item1.split("\n")
+            count.update(data)
+        return count
